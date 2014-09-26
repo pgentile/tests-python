eacf3ec9d9917b62d7739608b853b06af8fe508e
Pierre Gentile <pierre.gentile.perso@gmail.com>
2014-09-26 19:39:43 +0200

    Adresse de bind, possibilité de définir la hiérarchie d'exceptions catchée par silently

--- forking-server/forking_server_mp.py
+++ forking-server/forking_server_mp.py
@@ -37,7 +37,7 @@ def accept_and_handle(accept_socket, num):
         num,
         addr,
     )
-    with closing(connection):
+    with closing(silently(connection)):
         handle(connection, addr, num)
 
 
@@ -58,40 +58,45 @@ def handle(connection, addr, num):
 
 class SilentCaller(object):
 
-    __slots__ = ('__obj', )
+    __slots__ = ('__obj', '__root')
 
-    def __init__(self, obj):
+    def __init__(self, obj, root=Exception):
         super(SilentCaller, self).__init__()
         self.__obj = obj
+        self.__root = root
 
     def __getattr__(self, name):
         method = getattr(self.__obj, name)
+        if not callable(method):
+            raise ValueError("{!r} is not callable".format(method))
+            
         def wrapper(*args, **kwargs):
             try:
                 return method(*args, **kwargs)
-            except:
+            except self.__root:
                 pass
         return wrapper
 
 
-def silently(obj):
-    return SilentCaller(obj)
+def silently(*args, **kwargs):
+    return SilentCaller(*args, **kwargs)
 
 
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser(description=u'Lancer un serveur en mode fork')
     arg_parser.add_argument('port', type=int, help=u'Port')
-    arg_parser.add_argument('--workers', type=int, help=u'Nombre de workers', default=5)
+    arg_parser.add_argument('--workers', '-w', type=int, help=u'Nombre de workers', default=5)
     arg_parser.add_argument('--backlog', type=int, help=u'Taille du backlog de connexions', default=0)
+    arg_parser.add_argument('--bind', '-b', help=u'Adresse sur laquelle écouter', default='0.0.0.0')
     args = arg_parser.parse_args()
     
     accept_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    with closing(accept_socket):
+    with closing(silently(accept_socket)):
         accept_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        accept_socket.bind(('', args.port))
+        accept_socket.bind((args.bind, args.port))
         accept_socket.listen(args.backlog)
         
-        print 'LISTENING [PID={:05d}] Port = {}'.format(os.getpid(), args.port)
+        print 'LISTENING [PID={:05d}] Port = {}, bind to {}'.format(os.getpid(), args.port, args.bind)
 
         processes = []
         for num in xrange(1, 1 + args.workers):
@@ -100,8 +105,14 @@ if __name__ == '__main__':
         
         try:
             for num, p in enumerate(processes, start=1):
-                print 'STARING [PID={:05d}, NUM=#{:02d}]'.format(os.getpid(), num)
+                print 'STARING WORKER #{1:02d} [PID={0:05d}]'.format(os.getpid(), num)
                 p.start()
         finally:
-            for p in processes:
-                silently(p).join()
+            timeout = 0.5
+            while processes:
+                for index, p in enumerate(processes):
+                    silently(p, root=BaseException).join(timeout)
+                    if not p.is_alive():
+                        print 'STOPPED WORKER WITH PID {1:05d} [PID={0:05d}]'.format(os.getpid(), p.pid)
+                        del processes[index]
+                        break
